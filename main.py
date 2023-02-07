c2e09ff6dc89e60b69ba310a6f866fede4bbf7b4
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-07 17:00:39 +0400

    Складываем загруженные файлы в папку images

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import requests
+from pathlib import Path
 
-def download_image(filename, url):
+
+def download_file(file_name, url):
+    path_to_save_files = Path(f'images/{file_name}')
+    path_to_save_files.parent.mkdir(parents=True, exist_ok=True)
+
     response = requests.get(url)
     response.raise_for_status()
-    with open(filename, 'wb') as file:
+
+    with path_to_save_files.open('wb') as file:
         file.write(response.content)
-    return
 
 
 def main():
-    filename = ' hubble.jpeg'
+    file_name = 'hubble.jpeg'
     url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    download_image(filename, url)
+    download_file(file_name, url)
 
 
 if __name__ == '__main__':
