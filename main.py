779edd06dd3fb68fd012b148eb9ee2c49b87e381
SGKespace <55636018+SGKespace@users.noreply.github.com>
2023-02-07 16:18:45 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+def download_image(filename, url):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filename, 'wb') as file:
+        file.write(response.content)
+    return
+
+
+def main():
+    filename = ' hubble.jpeg'
+    url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+    download_image(filename, url)
+
+
+if __name__ == '__main__':
+    main()
