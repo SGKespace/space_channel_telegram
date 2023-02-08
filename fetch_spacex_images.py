277ea5906395637390c61078d2c1c2b7bb099628
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-08 23:06:55 +0400

    Модуль spacex

---
+++ fetch_spacex_images.py
@@ -0,0 +1,30 @@
+import requests
+from pathlib import Path
+import urllib
+import os
+
+
+def fetch_spacex_last_launch(launche_id):  # Весь код, относящийся к скачиванию фотографий от SpaceX
+    if not launche_id:
+        api_url = "https://api.spacexdata.com/v5/launches/latest"
+    else:
+        api_url = f"https://api.spacexdata.com/v5/launches/{launche_id}"
+    response = requests.get(api_url)
+    response.raise_for_status()
+    urls = response.json()['links']['flickr']['original']
+    basis_file_name = response.json()['id'] + "_"
+    if not urls:
+        print('Ссылки еще не выложили')
+    else:
+        for urls_number, url in enumerate(urls):
+            path_to_save_files = Path(f'images/{basis_file_name + str(urls_number)}.jpeg')
+            path_to_save_files.parent.mkdir(parents=True, exist_ok=True)
+            photo_response = requests.get(url)
+            photo_response.raise_for_status()
+            with path_to_save_files.open('wb') as file:
+                file.write(photo_response.content)
+
+
+if __name__ == '__main__':
+    fetch_spacex_last_launch('5eb87d42ffd86e000604b384')
+    print('Тут только функции')
