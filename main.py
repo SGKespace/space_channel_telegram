6cf914bf14df6bb049b24000e350d1252088fdf1
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-08 14:38:01 +0400

    Грузим с NASA случайные картинки

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from pathlib import Path
 import urllib
 import os
+from dotenv import load_dotenv
+
 
 def fetch_spacex_last_launch():  # Весь код, относящийся к скачиванию фотографий от SpaceX
     api_url = "https://api.spacexdata.com/v5/launches/latest"
@@ -19,17 +21,41 @@ def fetch_spacex_last_launch():  # Весь код, относящийся к с
                 file.write(response.content)
 
 
-def return_extension(url):
+def return_pars_name(url):
     spliten_url = urllib.parse.urlsplit(url)
-    (file_name, file_extension) = os.path.splitext(spliten_url.path)
-    return file_extension
+    (full_path, full_name) = os.path.split(spliten_url.path)
+    return os.path.splitext(full_name)
+
+
+def fetch_nasa_best_image(nasa_token, count):
+    request_url = 'https://api.nasa.gov/planetary/apod'
+    params = {'api_key': nasa_token, 'count': count}
+    try:
+        response = requests.get(request_url, params=params)
+        response.raise_for_status()
+
+        for urls_number, current_response in enumerate(response.json()):
+            url = current_response['hdurl']
+            (file_name, file_extension) = return_pars_name(url)
+            path_to_save_files = Path(f'nasa/{file_name}{file_extension}')
+            path_to_save_files.parent.mkdir(parents=True, exist_ok=True)
+            photo_response= requests.get(url)
+            photo_response.raise_for_status()
+            with path_to_save_files.open('wb') as file:
+                file.write(photo_response.content)
+
+    except requests.exceptions.HTTPError:
+        print('Вы ввели неверный токен или сформировался неверный запрос.')
+    except requests.exceptions.ConnectionError:
+        print('ConnectionError: Не могу подключиться к серверу.')
+    return
 
 
 def main():
-   # fetch_spacex_last_launch()
-   url = "https://example.com/txt/hello%20world.txt?v=9#python"
-   file_extension = return_extension(url)
-
+    load_dotenv()
+    nasa_token = os.environ["NASA_TOKEN"]
+    fetch_nasa_best_image(nasa_token, 2)  # грузим 2 лучших картинки случайным образом
+    fetch_spacex_last_launch()
 
 if __name__ == '__main__':
     main()
