6afd68335c96dc45ebc59ea1a221d16d67d9efb6
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-08 17:51:05 +0400

    Загружаем эпичные картинки

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import urllib
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 
 def fetch_spacex_last_launch():  # Весь код, относящийся к скачиванию фотографий от SpaceX
@@ -27,23 +28,47 @@ def return_pars_name(url):
     return os.path.splitext(full_name)
 
 
+def fetch_nasa_earth(nasa_token, count):  # типа красивые картинки земли, вывести надо 5-10 ()
+    request_url = 'https://api.nasa.gov/EPIC/api/natural/'
+    params = {'api_key': nasa_token}
+    try:
+        response = requests.get(request_url, params=params)
+        response.raise_for_status()
+        urls = []
+        for item in response.json():
+            photo_name = item['image']
+            found_date = datetime.strptime(item['date'], '%Y-%m-%d  %H:%M:%S')
+            formatted_date = f'{found_date.year}/{"{:02d}".format(found_date.month)}/{"{:02d}".format(found_date.day)}'
+            urls.append(f'https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{photo_name}.png')
+
+        for url in urls[:count]:
+            (file_name, file_extension) = return_pars_name(url)
+            path_to_save_files = Path(f'nasa/{file_name}{file_extension}')
+            path_to_save_files.parent.mkdir(parents=True, exist_ok=True)
+            photo_response = requests.get(url, params=params)
+            photo_response.raise_for_status()
+            with path_to_save_files.open('wb') as file:
+                file.write(photo_response.content)
+    except requests.exceptions.HTTPError:
+        print('Вы ввели неверный токен или сформировался неверный запрос.')
+
+
+
 def fetch_nasa_best_image(nasa_token, count):
     request_url = 'https://api.nasa.gov/planetary/apod'
     params = {'api_key': nasa_token, 'count': count}
     try:
         response = requests.get(request_url, params=params)
         response.raise_for_status()
-
         for urls_number, current_response in enumerate(response.json()):
             url = current_response['hdurl']
             (file_name, file_extension) = return_pars_name(url)
             path_to_save_files = Path(f'nasa/{file_name}{file_extension}')
             path_to_save_files.parent.mkdir(parents=True, exist_ok=True)
-            photo_response= requests.get(url)
+            photo_response = requests.get(url)
             photo_response.raise_for_status()
             with path_to_save_files.open('wb') as file:
                 file.write(photo_response.content)
-
     except requests.exceptions.HTTPError:
         print('Вы ввели неверный токен или сформировался неверный запрос.')
     except requests.exceptions.ConnectionError:
@@ -56,6 +81,7 @@ def main():
     nasa_token = os.environ["NASA_TOKEN"]
     fetch_nasa_best_image(nasa_token, 2)  # грузим 2 лучших картинки случайным образом
     fetch_spacex_last_launch()
+    fetch_nasa_earth(nasa_token, 3)  # грузим 3  картинки
 
 if __name__ == '__main__':
     main()
