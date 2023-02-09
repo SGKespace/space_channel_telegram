c23cb9fdbd7206b76f0348a2069a24005cc274a7
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-09 14:47:44 +0400

    публикуем файлы в случайном порядке
    вообще такое решение зло и обман пользователей канала

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,15 +2,18 @@ import telegram
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+import random
 
 
 def main(bot, chat_id, images_spacex_path, images_nasa_path):
     # bot.send_message(chat_id=chat_id, text="лалалалала")
     for root, directory, files in os.walk(images_nasa_path):
+        random.shuffle(files)
         for file in files:
             bot.sendPhoto(chat_id=chat_id, photo=open(f'{images_nasa_path}/{file}', 'rb'),)
 
     for root, directory, files in os.walk(images_spacex_path):
+        random.shuffle(files)
         for file in files:
             bot.sendPhoto(chat_id=chat_id, photo=open(f'{images_spacex_path}/{file}', 'rb'),)
 
@@ -25,6 +28,8 @@ if __name__ == '__main__':
     chat_id = os.environ["TELEGRAM_CHAT_ID"]
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     sleep_time = os.environ["SLEEP_TIME"]
+    if not sleep_time:
+        sleep_time = 60*60*4  # 4 часа
 
     while True:
         main(bot, chat_id, images_spacex_path, images_nasa_path)
