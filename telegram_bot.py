6436277c649623aeff25157d47a87c480ac71b50
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-09 13:49:38 +0400

    запускаем с задержкой из .env (привет асинху :-) )

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,10 +5,16 @@ from pathlib import Path
 
 
 def main(bot, chat_id, images_spacex_path, images_nasa_path):
-    bot.send_message(chat_id=chat_id, text="лалалалала")
+    # bot.send_message(chat_id=chat_id, text="лалалалала")
     for root, directory, files in os.walk(images_nasa_path):
         for file in files:
             bot.sendPhoto(chat_id=chat_id, photo=open(f'{images_nasa_path}/{file}', 'rb'),)
+
+    for root, directory, files in os.walk(images_spacex_path):
+        for file in files:
+            bot.sendPhoto(chat_id=chat_id, photo=open(f'{images_spacex_path}/{file}', 'rb'),)
+
+
 
 
 if __name__ == '__main__':
@@ -18,4 +24,8 @@ if __name__ == '__main__':
     TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
     chat_id = os.environ["TELEGRAM_CHAT_ID"]
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    main(bot, chat_id, images_spacex_path, images_nasa_path)
+    sleep_time = os.environ["SLEEP_TIME"]
+
+    while True:
+        main(bot, chat_id, images_spacex_path, images_nasa_path)
+        time.sleep(sleep_time)
