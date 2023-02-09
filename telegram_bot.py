b3baaa7349930faf45aaa1ec8155b88b2eef1277
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-09 15:27:05 +0400

    исправлены ошибки

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,34 +3,35 @@ from dotenv import load_dotenv
 import os
 from pathlib import Path
 import random
+import time
 
 
-def main(bot, chat_id, images_spacex_path, images_nasa_path):
+def send_telegram_image(bot, chat_id, images_spacex_path, images_nasa_path):
     # bot.send_message(chat_id=chat_id, text="лалалалала")
     for root, directory, files in os.walk(images_nasa_path):
         random.shuffle(files)
         for file in files:
             bot.sendPhoto(chat_id=chat_id, photo=open(f'{images_nasa_path}/{file}', 'rb'),)
-
     for root, directory, files in os.walk(images_spacex_path):
         random.shuffle(files)
         for file in files:
             bot.sendPhoto(chat_id=chat_id, photo=open(f'{images_spacex_path}/{file}', 'rb'),)
 
 
-
-
-if __name__ == '__main__':
+def main():
     images_spacex_path = Path('images/')
     images_nasa_path = Path('nasa/')
     load_dotenv()
     TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
     chat_id = os.environ["TELEGRAM_CHAT_ID"]
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    sleep_time = os.environ["SLEEP_TIME"]
+    sleep_time = int(os.environ["SLEEP_TIME"])
     if not sleep_time:
-        sleep_time = 60*60*4  # 4 часа
-
+        sleep_time = str(60 * 60 * 4)  # 4 часа
     while True:
-        main(bot, chat_id, images_spacex_path, images_nasa_path)
+        send_telegram_image(bot, chat_id, images_spacex_path, images_nasa_path)
         time.sleep(sleep_time)
+
+
+if __name__ == '__main__':
+    main()
