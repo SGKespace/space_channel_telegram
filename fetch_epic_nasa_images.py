acf37992a3679caa4580e7354819159f659ca3e4
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-10 14:15:12 +0400

    уточнено название переменной

--- fetch_epic_nasa_images.py
+++ fetch_epic_nasa_images.py
@@ -14,9 +14,9 @@ def fetch_nasa_earth(nasa_token, count):  # типа красивые карти
         response = requests.get(request_url, params=params)
         response.raise_for_status()
         urls = []
-        for current_response in response.json():
-            photo_name = current_response['image']
-            found_date = datetime.strptime(current_response['date'], '%Y-%m-%d  %H:%M:%S')
+        for request in response.json():
+            photo_name = request['image']
+            found_date = datetime.strptime(request['date'], '%Y-%m-%d  %H:%M:%S')
             formatted_date = f'{found_date.year}/{"{:02d}".format(found_date.month)}/{"{:02d}".format(found_date.day)}'
             urls.append(f'https://api.nasa.gov/EPIC/archive/natural/{formatted_date}/png/{photo_name}.png')
         if len(urls) < count:
