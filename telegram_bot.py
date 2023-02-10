5a37d093e442f040094bf319299d665e94a99ada
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-10 15:29:41 +0400

    добавлена функция

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,10 +8,17 @@ import time
 
 def send_telegram_images(bot, chat_id, images_path):
     for root, directory, files in os.walk(images_path):
-        random.shuffle(files)
         for file in files:
             with open(f'{images_path}/{file}', 'rb') as fi:
                 bot.sendPhoto(chat_id=chat_id, photo=fi)
+
+
+def re_send_telegram_images(bot, chat_id, images_path):
+    for root, directory, files in os.walk(images_path):
+        random.shuffle(files)
+        file = files[1]
+        with open(f'{images_path}/{file}', 'rb') as fi:
+            bot.sendPhoto(chat_id=chat_id, photo=fi)
 
 
 def main():
@@ -21,13 +28,11 @@ def main():
     telegram_token = os.environ["TELEGRAM_TOKEN"]
     chat_id = os.environ["TELEGRAM_CHAT_ID"]
     bot = telegram.Bot(token=telegram_token)
-    sleep_time = int(os.environ["SLEEP_TIME"])
-    if not sleep_time:
-        sleep_time = str(60 * 60 * 4)  # 4 часа
-    while True:
-        send_telegram_images(bot, chat_id, images_spacex_path)
-        send_telegram_images(bot, chat_id, images_nasa_path)
-        time.sleep(sleep_time)
+
+    send_telegram_images(bot, chat_id, images_spacex_path)
+    send_telegram_images(bot, chat_id, images_nasa_path)
+
+
 
 
 if __name__ == '__main__':
