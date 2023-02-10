1d433a3e11f2ad1f2ae3a2a833f4c5d54e51020d
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-10 13:53:17 +0400

    исправлена функция загрузки - добавлен параметр

--- common_helper_functions.py
+++ common_helper_functions.py
@@ -1,13 +1,16 @@
 import urllib
 import os
+import requests
+
 
 def return_pars_name(url):
     spliten_url = urllib.parse.urlsplit(url)
     (full_path, full_name) = os.path.split(spliten_url.path)
     return os.path.splitext(full_name)
 
-def download_files(url, path_to_save_files):
-    photo_response = requests.get(url)
+
+def download_files(url, path_to_save_files, params: str = ''):
+    photo_response = requests.get(url, params=params)
     photo_response.raise_for_status()
     with path_to_save_files.open('wb') as file:
         file.write(photo_response.content)
